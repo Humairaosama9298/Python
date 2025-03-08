2bfa6af17ac8650d5060119f55da8dae57dbc3c5
Huma_aira@Osama <142533254+Humairaosama9298@users.noreply.github.com>
2025-03-09 00:07:33 +0500

    Delete python_challenge directory

--- python_challenge/daily_python_challenge/task1.py
+++
@@ -1,9 +0,0 @@
-#                      ***** Task 1 *****
-
-#            ***** Print Hello World Message *****
-
-message = "Welcome to Python Programming.Let's Start coding."
-print("Hello World", message)
-
-#                   ***** End of Task 1 *****
-
